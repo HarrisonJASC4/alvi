328e7debb9ed0b3aa2065aea1a2c0f7e618cb1d2
alviasc4 <alvirahman365@gmail.com>
2017-07-12 22:26:24 -0700

    alvir.js

---
+++ RPS.js
@@ -0,0 +1,15 @@
+var playerchoice = "Rock";
+var compchoices = ["Rock"; "Paper"; "Scissors"]
+var compChoice = compChois=ces[Math.floor(Math.random()*3]
+if (compChoice = "Rock" && playerChoice == "Rock"){
+    console.log("It's a tie")
+}
+else if (compChoice == "Paper" && playerChoice == "Rock"){
+    console.log("You Lose");
+}
+else if (compChoice == "Scissors" && playerChoice == "Paper"){
+    console.log("You Lose");
+}
+else if (compChoice == "Paper" && playerChoice == "Scissors"){
+    console.log("You Lose");
+}
---
+++ function.js
@@ -0,0 +1,12 @@
+// function wellohorld (){
+//     console.log("Wello, horld");
+//     return ("Wello, horld")
+// }
+
+// var result = wellohorld();
+// console.log(result);
+
+function hello(name) {
+    console.log("hello, " + name + "!");
+}
+hello("Alvi");
